7e1c3469ea50e8d0345edfdebef5120607d626f0
Ocheezyy <miniod2018@gmail.com>
2021-03-01 16:58:40 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Char from './Char/Char';
 
-function App() {
+
+const TextValidator = (props) => {
+
+  const longEnough = props.length >= 5 ? <p style={{color: 'green'}}>Text long enough</p> : <p style={{color: 'red'}}>Text too short!</p>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{longEnough}</p>
     </div>
   );
+};
+
+class App extends Component {
+  state = {
+    inputValue: '',
+    inputLength: 0,
+    inputArr: []
+  };
+
+  inputChangeHandler = (event) => {
+    this.setState({
+      inputValue: event.target.value,
+      inputLength: event.target.value.length,
+      inputArr: event.target.value.split("")
+    });
+  };
+
+  // charClickHandler = (event) => {
+  //   const input = [...this.state.inputValue];
+  //   input.findIndex(char => {
+  //     char === event.
+  //   })
+  //   this.setState({
+
+  //   })
+  // }
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Second practice Task</h1>
+        <input type="text" value={this.state.inputValue} onChange={(event) => this.inputChangeHandler(event)} />
+        {this.state.inputLength > 0 ? <p>Text Length: {this.state.inputLength}</p> : null}
+        <TextValidator length={this.state.inputLength} />
+        {this.state.inputArr.map(val => {
+          return <Char char={val} />
+        })}
+      </div>
+    );
+  };
+
 }
+
+
+
+
 
 export default App;
---
+++ src/Char/Char.js
@@ -0,0 +1,10 @@
+import './Char.css';
+
+
+const Char = (props) => {
+    <div className='char-div'>
+        {props.char}
+    </div>
+}
+
+export default Char;
